e8bd9293ad19daa409a6f9d03d4091a52ceff839
Aradhya Maheshwari <maheshwariaradhya065@gmail.com>
2026-07-12 08:08:58 +0530

    Complete terminal-based calculator

---
+++ main.py
@@ -0,0 +1,11 @@
+from main_menu import main_menu
+
+def main():
+    print("="*30)
+    text = "Simple Calculator"
+    print(f"{text:^30}")
+    print("="*30)
+    main_menu()
+
+if __name__ == "__main__":
+    main()
--- main_menu.py
+++ main_menu.py
@@ -1,3 +1,12 @@
+import utils.validate
+import modules.operations
+
+menu_actions = {
+    "1" : modules.operations.add,
+    "2" : modules.operations.subtract,
+    "3" : modules.operations.multiply,
+    "4" : modules.operations.divide,
+}
 
 def print_main_menu():
     print("1. Addition")
@@ -9,4 +18,26 @@ def print_main_menu():
 def main_menu():
     while True:
         print_main_menu()
-        break
+
+        choice = input("Enter your choice: ")
+
+        if not utils.validate.validate_menu_choice(choice):
+            print("Please enter a valid choice (1-5): ")
+            continue
+        if choice == "5":
+            print("Thank you for using the calculator!")
+            break
+        else:
+            a = input("Enter first number: ")
+            b = input("Enter second number: ")
+            if not utils.validate.validate_operands(a, b):
+                print("Invalid input. Please enter numeric values.")
+                continue
+            a = float(a)
+            b = float(b)
+        try:
+            result = menu_actions[choice](a, b)
+            print(f"Result: {result}")
+            print()
+        except ZeroDivisionError as e:
+            print(e)
