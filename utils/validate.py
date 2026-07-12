ab13dc5642b080c77b00e1589abbe0b5b89acb02
Aradhya Maheshwari <maheshwariaradhya065@gmail.com>
2026-07-12 08:05:06 +0530

    Add input validation

---
+++ utils/validate.py
@@ -0,0 +1,10 @@
+def validate_menu_choice(choice):
+    return choice in {"1", "2", "3", "4", "5"}
+
+def validate_operands(a, b):
+    try:
+        float(a)
+        float(b)
+        return True
+    except ValueError:
+        return False
