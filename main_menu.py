cb3ceb116486b7c282d2663f3498ea6bcd237ba7
Aradhya Maheshwari <maheshwariaradhya065@gmail.com>
2026-07-12 07:57:26 +0530

    Add calculator menu

---
+++ main_menu.py
@@ -0,0 +1,12 @@
+
+def print_main_menu():
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+def main_menu():
+    while True:
+        print_main_menu()
+        break
