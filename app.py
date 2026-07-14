4a082d0910bbde3c63b37424b4d558bc4b88ebde
Aradhya Maheshwari <maheshwariaradhya065@gmail.com>
2026-07-14 19:24:52 +0530

    Create Tkinter application window

---
+++ app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Calculator")
+root.geometry("420x320")
+
+title = tk.Label(root, text="Simple Calculator")
+title.pack(pady=10)
+
+root.mainloop()
